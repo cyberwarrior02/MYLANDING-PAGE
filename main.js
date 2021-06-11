725032547af121a5955208229f4b02097be11e8e
cyberwarrior02 <bhavyachopra2509@gmail.com>
2021-06-11 23:11:31 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleDropdown(){
+var x=document.getElementById("topnav-toggle");
+if(x.className==="topnav"){
+    x.className+=" responsive";}
+    else{
+        x.className="topnav";
+    }
+}
